da968f8ec6e5db27234f7a0d2ade3b7718a71abb
monikava <monikavaliauskaite@gmail.com>
2025-03-10 11:46:51 +0200

    nauji sprendimai

--- main.py
+++ main.py
@@ -3,60 +3,93 @@ import random
 print("-----------LENGVESNI-----------\n")
 # 1. Sukurkite Funkciją kuri priima du int tipo kintamuosius. Juos susumuoja ir atspausdina.
 print("1. Sukurkite Funkciją kuri priima du int tipo kintamuosius. Juos susumuoja ir atspausdina.")
-def two_numbers(a,b):
+def two_numbers(a, b):
     print(a + b)
-two_numbers(4,14)
+
+two_numbers(4, 14)
 
 # 2. Sukurkite Funkciją kuri vadinasi PISq. Funkcija gražina float tipo reikšmę. Reikšmė yra : 9.8596; Gautą reikšmę atspausdinkite.
-print("\n2. Sukurkite Funkciją kuri vadinasi PISq. Funkcija gražina float tipo reikšmę. Reikšmė yra : 9.8596; Gautą reikšmę atspausdinkite.")
-def PISq ():
+print(
+    "\n2. Sukurkite Funkciją kuri vadinasi PISq. Funkcija gražina float tipo reikšmę. Reikšmė yra : 9.8596; Gautą reikšmę atspausdinkite.")
+def PISq():
     f = float(9.8596)
     return f
+
 print(PISq())
 
 # 3. Sukurkite Funkciją kuri priima du int tipo kintamuosius. Funkcija gražina skaičių sandaugą.; Gautą reikšmę atspausdinkite.
-print("\n3. Sukurkite Funkciją kuri priima du int tipo kintamuosius. Funkcija gražina skaičių sandaugą.; Gautą reikšmę atspausdinkite.")
-def multiply(a,b):
+print(
+    "\n3. Sukurkite Funkciją kuri priima du int tipo kintamuosius. Funkcija gražina skaičių sandaugą.; Gautą reikšmę atspausdinkite.")
+def multiply(a, b):
     c = a * b
     return c
 
-print(multiply(5,5))
+print(multiply(5, 5))
 
 # 4. Sukurkite Funkciją kuri priima masyvą, prasuka ciklą ir atspausdina kiekvieną narį vienoje eilutėje.
 print("\n4. Sukurkite Funkciją kuri priima masyvą, prasuka ciklą ir atspausdina kiekvieną narį vienoje eilutėje.")
-
 def print_list(array):
     row = ""
     for a in array:
         row += (f"{a} ")
     print(row)
 
-array = [1,3,5,7,9,11]
+array = [1, 3, 5, 7, 9, 11]
 print_list(array)
 
 # 5. Sukurkite Funkciją kuri priima du int tipo kintamuosius, min ir max reikšmėms nustatyti ir sugeneruoja random int skaičių ir jį gražintų.
-print("\n5. Sukurkite Funkciją kuri priima du int tipo kintamuosius, min ir max reikšmėms nustatyti ir sugeneruoja random int skaičių ir jį gražintų.")
-def min_max(min,max):
-    return(random.randint(min,max))
+print(
+    "\n5. Sukurkite Funkciją kuri priima du int tipo kintamuosius, min ir max reikšmėms nustatyti ir sugeneruoja random int skaičių ir jį gražintų.")
+def min_max(min, max):
+    return (random.randint(min, max))
 
 print(min_max(0, 999))
 
 # 6. Sukurkite Funkciją kuri sugeneruotų random int skaičių masyvą ir jį gražintų. Funkcija priima tris int tipo kintamuosius, min, max ir length reikšmėms nustatyti.
-print(f"\n6. Sukurkite Funkciją kuri sugeneruotų random int skaičių masyvą ir jį gražintų. Funkcija priima tris int tipo kintamuosius, min, max ir length reikšmėms nustatyti.")
-def random_array(min,max,length):
-    random.randint(min,max,length)
-    return(array)
+print(
+    f"\n6. Sukurkite Funkciją kuri sugeneruotų random int skaičių masyvą ir jį gražintų. Funkcija priima tris int tipo kintamuosius, min, max ir length reikšmėms nustatyti.")
+def random_array(min, max, length):
+    array2 = []
+    for a in range(0, length):
+        a = random.randint(min, max)
+        array2.append(a)
+    return (array2)
 
-
-
+print(random_array(1, 5, 3))
 
 # 7. Sukurkite Funkciją kuri panaudotų 6-toje užduotyje sugeneruotą masyvą (priimtų kaip kintamąjį), susumuotų ir gražintų reikšmę.
+print(
+    "\n7. Sukurkite Funkciją kuri panaudotų 6-toje užduotyje sugeneruotą masyvą (priimtų kaip kintamąjį), susumuotų ir gražintų reikšmę.")
+def sum_array(random_array):
+    sumarray = 0
+    for num in random_array:
+        sumarray = sumarray + num
+    return (sumarray)
 
+print(sum_array(random_array(1, 6, 10)))
 
 # 8. Sukurkite Funkciją kuri priimtų 6toje užduotyje sugeneruotą masyvą ir gražintų jos skaičių vidurkį (double).
+print(
+    "\n8. Sukurkite Funkciją kuri priimtų 6toje užduotyje sugeneruotą masyvą ir gražintų jos skaičių vidurkį (double).")
+def array_average(random_array):
 
+    arrayaverage = 0
+    sumarray = 0
+    length = 0
+    for n in random_array:
+        sumarray = sumarray + n
+        length += 1
+        arrayaverage = float(sumarray / length)
+    return (arrayaverage)
+
+print(array_average(random_array(1, 10, 6)))
 
 # 9. Sukurkite Funkciją kuri priimtų du int skaičius ir atspausdintų stačiakampį užpildytą žvaigždutėmis. Pirmas int - išoriniam ciklui, antras vidiniam.
+print("\n9. Sukurkite Funkciją kuri priimtų du int skaičius ir atspausdintų stačiakampį užpildytą žvaigždutėmis. Pirmas int - išoriniam ciklui, antras vidiniam.")
+def rectangle(length,width):
+    for l in range(1,length):
+        for w in range(1,width):
+
 
 
 # 10. Sukurkite Funkciją kuri priimtų sakinį kaip kintamąjį ir atspausdintų kiek jame yra raidžių(simbolių) ir tarpų. Sakinys - Šiandien labai graži diena”. (kodas turi veikti padavus bet kokį sakinį) (simboliu yra 23, tarpu yra 3)
@@ -85,9 +118,3 @@ def random_array(min,max,length):
 # 7. Suskaičiuokite šešto uždavinio elementų, kurie nėra masyvai, sumą. Skaičiuoti reikia visuose masyvuose ir submasyvuose.
 # 8. Sugeneruokite masyvą iš trijų elementų, kurie yra atsitiktiniai skaičiai nuo 1 iki 33. Jeigu tarp trijų paskutinių elementų yra nors vienas ne pirminis skaičius, prie masyvo pridėkite dar vieną elementą- atsitiktinį skaičių nuo 1 iki 33. Vėl patikrinkite pradinę sąlygą ir jeigu reikia pridėkite dar vieną elementą. Kartokite, kol sąlyga nereikalaus pridėti elemento. `
 # 9. Sugeneruokite masyvą iš 10 elementų, kurie yra masyvai iš 10 elementų, kurie yra atsitiktiniai skaičiai nuo 1 iki 100. Jeigu tokio didelio masyvo (ne atskirai mažesnių) pirminių skaičių vidurkis mažesnis už 70, suraskite masyve mažiausią skaičių (nebūtinai pirminį) ir prie jo pridėkite 3. Vėl paskaičiuokite masyvo pirminių skaičių vidurkį ir jeigu mažesnis nei 70 viską kartokite.
-
-
-
-
-
-
